c284fbf2210562e88c1d0199ca99fe80a508d9b7
YijieYang <quic_yijiyang@quicinc.com>
2024-03-22 10:51:07 +0800

    Support sync/compile/verify on specific tag
    Signed-off-by: YijieYang <quic_yijiyang@quicinc.com>

--- sheepdog-slave.py
+++ sheepdog-slave.py
@@ -41,6 +41,7 @@ def sync_code():
     remote_branch = config["REPO"]["branch"]
     repo_name = config["REPO"]["name"]
     local_repo_path = args.local
+    tag = config['REPO']['tag']
 
     if local_repo_path != None:
         repo = git.Repo(local_repo_path)
@@ -71,6 +72,8 @@ def sync_code():
         repo.head.reference = branch
 
     exec_shell_cmd(f"git fetch --tags {repo_name}")
+    exec_shell_cmd(f'git checkout {tag}')
+
     logging.info("sync_code finished")
 
 
