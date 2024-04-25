5cc571fa01f8456bde449406b08d8cbde1423abe
Yijie Yang <quic_yijiyang@quicinc.com>
2024-04-25 11:02:47 +0800

    Build efi.bin for systemd-boot platforms
    Some platforms such as AIM series use efi.bin instead of boot.img.
    So efi.bin building function should be added to this tool.
    Esdk is essential in building efi.bin, so it's extraction and installation
    process is involved in this patch. It should be noted that some tools may
    evolve over time, however the version of esdk was fixed in this patch and
    this is a workaround. It should be optimized later.
    
    Signed-off-by: Yijie Yang <quic_yijiyang@quicinc.com>

--- sheepdog-slave.py
+++ sheepdog-slave.py
@@ -2,7 +2,7 @@
 
 import sys, os, datetime, logging, configparser, argparse
 import subprocess, multiprocessing
-import glob, git
+import glob, git, shutil, re
 
 
 def exit_with_msg(msg, code):
@@ -28,7 +28,9 @@ def exec_shell_cmd(cmd):
     if ret_code == 0:
         logging.info(out)
     else:
-        raise Exception(f"Error executing command {cmd}. return ${ret_code}", ret_code)
+        raise Exception(
+            f"Error executing command {cmd}\nReturn code: ${ret_code}", ret_code
+        )
 
     return result
 
@@ -79,7 +81,7 @@ def sync_kernel():
         local_branch = repo.create_head(f"{repo_name}-{remote_branch}")
         local_branch.set_tracking_branch(remote.refs[remote_branch])
         repo.head.reference = local_branch
-    elif repo.active_branch.name != branch.name:
+    else:
         repo.head.reference = branch
 
     exec_shell_cmd(f"git fetch --tags {repo_name}")
@@ -104,13 +106,13 @@ def sync_mkbootimg():
     if mkbootimg != None and len(mkbootimg) != 0:
         return
 
-    repo = git.Repo.clone_from(url=mkbootimg_url, to_path=f"{workspace}/{repo_name}")
+    repo = git.Repo.clone_from(url=mkbootimg_url, to_path=f"{tool_path}/{repo_name}")
     os.chdir(repo.working_dir)
     local_branch = repo.create_head(target_branch)
     local_branch.set_tracking_branch(repo.refs[f"origin/{target_branch}"])
     repo.head.reference = local_branch
 
-    mkbootimg = f"{workspace}/{repo_name}/mkbootimg.py"
+    mkbootimg = f"{tool_path}/{repo_name}/mkbootimg.py"
 
     os.chdir(workspace)
     logging.info("sync mkbootimg finished")
@@ -119,6 +121,144 @@ def sync_mkbootimg():
 def sync_code():
     sync_mkbootimg()
     sync_kernel()
+    sync_build_tool()
+    install_esdk()
+
+
+def build_boot_image(kernel_components):
+    logging.info("build boot image")
+
+    cmd = mkbootimg
+    cmd += " --kernel " + kernel_components["kernel_image"]
+    cmd += " --dtb " + kernel_components["dtb"][0]
+    cmd += " --cmdline " + '"' + kernel_components["cmdline"] + '"'
+    cmd += " --ramdisk " + kernel_components["ramdisk"]
+    cmd += " --base 0x80000000 --pagesize 4096"
+    cmd += f" --output {workspace}/boot.img"
+
+    try:
+        logging.debug(f"Build boot image command: {cmd}")
+        exec_shell_cmd(cmd)
+    except Exception as e:
+        exit_with_msg(str(e.args[0]), e.args[1])
+
+
+def install_esdk():
+    logging.info("Install esdk")
+
+    sdk_script = "/prj/qct/quic/oe_filer_scratch/ESDK/LE.QCLINUX.1.0/AU_LINUX_EMBEDDED_LE.QCLINUX.1.0_TARGET_ALL.01.013.236/qcom-wayland-x86_64-qcom-console-image-armv8-2a-qcm6490-toolchain-ext-1.0.sh"
+    sdk_path = f"{tool_path}/esdk"
+    cmd = f"{sdk_script} -y -d {sdk_path}"
+
+    if os.path.exists(sdk_path):
+        logging.info(f"Already installed, remove {sdk_path} to reinstall.")
+        return
+
+    logging.info(f"Install esdk to {sdk_path}")
+    try:
+        logging.debug(f"Install esdk command: {cmd}")
+        exec_shell_cmd(cmd)
+    except Exception as e:
+        exit_with_msg(str(e.args[0]), e.args[1])
+
+
+def sync_build_tool():
+    logging.info("sync build tool begin")
+
+    url = "ssh://git-android-sha-drekar.quicinc.com:29418/kernel/build"
+    branch = "qclinux.1.0"
+    repo_name = "build"
+    remote_name = "origin"
+    build_tool_path = f"{tool_path}/{repo_name}"
+
+    if os.path.exists(build_tool_path):
+        logging.info(f"Already installed, remove {build_tool_path} to reinstall.")
+        return
+
+    repo = git.Repo.clone_from(url=url, to_path=f"{tool_path}/{repo_name}")
+    os.chdir(repo.working_dir)
+    local_branch = repo.create_head(branch)
+    local_branch.set_tracking_branch(repo.refs[f"{remote_name}/{branch}"])
+    local_branch.checkout()
+    repo.remotes[remote_name].pull(rebase=True)
+
+    os.chdir(workspace)
+    logging.info("sync build tool finished")
+
+
+def modify_build_script(script):
+    """
+    'du' path in this script was fixed, we @don't use that, so modify it
+    """
+    with open(script, "r+") as file:
+        data = file.read()
+        data = re.sub("prebuilt.*du", "du", data)
+        file.seek(0)
+        file.write(data)
+        file.truncate()
+
+
+def build_efi_bin(kernel_components):
+    esdk_bin_path = tool_path + "/esdk/tmp/sysroots/x86_64/usr/bin"
+    esdk_img_path = tool_path + "/esdk/tmp/deploy/images/qcm6490"
+    esdk_lib_path = tool_path + "/esdk/tmp/sysroots/x86_64/usr/lib"
+
+    # mcopy = esdk_bin_path + "/mcopy"
+    ukify = esdk_bin_path + "/ukify"
+
+    systemd_img = esdk_img_path + "/systemd-bootaa64.efi"
+    efi_stub = esdk_img_path + "/linuxaa64.efi.stub"
+
+    efi_bin = workspace + "/efi.bin"
+    python_path = esdk_lib_path + "/python3.10/site-packages"
+
+    efi_dir = workspace + "/efi_dir"
+    gen_efi_boot = f"{tool_path}/build/generate_efi_boot.sh"
+
+    logging.info("build efi bin")
+
+    modify_build_script(gen_efi_boot)
+
+    if os.access(efi_bin, os.F_OK):
+        os.remove(efi_bin)
+    if os.path.exists(efi_dir):
+        shutil.rmtree(efi_dir)
+
+    os.makedirs(f"{efi_dir}/dtb")
+    os.makedirs(f"{efi_dir}/EFI/BOOT")
+    os.makedirs(f"{efi_dir}/EFI/Linux")
+    os.makedirs(f"{efi_dir}/loader")
+
+    uki_name = workspace + "/uki.efi"
+
+    for dtb in kernel_components["dtb"]:
+        shutil.copy(dtb, f"{efi_dir}/dtb")
+    shutil.copy(systemd_img, f"{efi_dir}/EFI/BOOT/bootaa64.efi")
+    open(f"{efi_dir}/loader/loader.conf", "w").close()
+
+    os.environ["PYTHONPATH"] = python_path
+    os.environ["PATH"] = f"{esdk_bin_path}:" + os.environ["PATH"]
+    logging.info(f'PYTHONPATH={os.environ["PYTHONPATH"]}')
+    logging.info(f'PATH={os.environ["PATH"]}')
+
+    uki_cmd = f"python {ukify} build"
+    uki_cmd += " --initrd=" + kernel_components["ramdisk"]
+    uki_cmd += " --linux=" + kernel_components["kernel_image"]
+    uki_cmd += " --efi-arch=aa64"
+    uki_cmd += " --cmdline=" + '"' + kernel_components["cmdline"] + '"'
+    uki_cmd += " --stub=" + efi_stub
+    uki_cmd += " --output=" + uki_name
+
+    try:
+        exec_shell_cmd(uki_cmd)
+        logging.info(f"UKI image created at {uki_name}")
+
+        shutil.copy(uki_name, f"{efi_dir}/EFI/Linux")
+
+        exec_shell_cmd(f"{gen_efi_boot} {efi_dir} {efi_bin}")
+        logging.info(f"EFI image created at {efi_bin}")
+    except Exception as e:
+        exit_with_msg(str(e.args[0]), e.args[1])
 
 
 def compile():
@@ -131,12 +271,10 @@ def compile():
     arch = dev_info["arch"]
     dev_name = dev_info["name"]
     ramdisk_url = dev_info["ramdisk_url"]
-    cmdline = dev_info["cmdline"]
     vendor = dev_info["vendor"]
 
     toolchain_prefix = tools["toolchain_prefix"]
 
-    ramdisk = f"{compile_path}/ramdisk.gz"
     make_options = f"-j{cpu_num} ARCH={arch} CROSS_COMPILE={toolchain_prefix}"
 
     options_kernel = kernel_options["kernel"].split()
@@ -144,9 +282,13 @@ def compile():
     options_close = kernel_options["close"].split()
 
     defconfig = f"{compile_path}/arch/{arch}/configs/defconfig"
-    image = f"arch/{arch}/boot/Image.gz"
-    dtb = f"arch/{arch}/boot/dts/{vendor}/{dev_name}.dtb"
-    bootimg = f"{workspace}/boot.img"
+
+    kernel_components = {
+        "kernel_image": f"{compile_path}/arch/{arch}/boot/Image",
+        "dtb": [f"{compile_path}/arch/{arch}/boot/dts/{vendor}/{dev_name}.dtb"],
+        "ramdisk": f"{compile_path}/ramdisk.gz",
+        "cmdline": dev_info["cmdline"],
+    }
 
     with open(defconfig, "a") as f:
         for option in options_kernel:
@@ -164,16 +306,15 @@ def compile():
         exec_shell_cmd(
             f"make {make_options} modules_install INSTALL_MOD_PATH=./modules_dir INSTALL_MOD_STRIP=1"
         )
-        exec_shell_cmd(f"cat {image} {dtb} > Image.gz+dtb")
 
-        if not os.access(ramdisk, os.F_OK):
-            exec_shell_cmd(f"wget -O {ramdisk} {ramdisk_url}")
-
-        exec_shell_cmd(
-            f'{mkbootimg} --kernel Image.gz+dtb --cmdline "{cmdline}" --ramdisk {ramdisk} --base 0x80000000 --pagesize 4096 --output {bootimg}'
-        )
+        if not os.access(kernel_components["ramdisk"], os.F_OK):
+            exec_shell_cmd(f"wget -O {kernel_components['ramdisk']} {ramdisk_url}")
     except Exception as e:
         exit_with_msg(str(e.args[0]), e.args[1])
+
+    build_boot_image(kernel_components)
+    install_esdk()
+    build_efi_bin(kernel_components)
 
 
 def precheck():
@@ -222,8 +363,9 @@ def parse_options():
 
 
 def env_init():
-    global workspace
+    global workspace, tool_path
     workspace = os.getcwd()
+    tool_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def initialize():
